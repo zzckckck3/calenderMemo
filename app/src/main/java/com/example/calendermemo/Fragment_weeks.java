32f8157c42b5e1b7b71f455e67cd5c41d2fecf72
Donggean Lim <ldavid0127@gmail.com>
2022-11-11 12:21:19 +0900

    1차 제작

---
+++ app/src/main/java/com/example/calendermemo/Fragment_weeks.java
@@ -0,0 +1,21 @@
+package com.example.calendermemo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment_weeks extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weeks, container, false);
+    }
+}
--- app/src/main/java/com/example/calendermemo/MainActivity.java
+++ app/src/main/java/com/example/calendermemo/MainActivity.java
@@ -1,14 +1,93 @@
 package com.example.calendermemo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 
 import android.os.Bundle;
 
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    Fragment_months fragment_months;
+    Fragment_weeks fragment_weeks;
+    Fragment_today fragment_today;
+    Fragment_day_add fragment_day_add;
+
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+
+    ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Calendar Memo"); //toolbar 제목
+
+        drawerLayout=findViewById(R.id.layout_drawer);
+        navigationView=findViewById(R.id.nav_bar);
+
+        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.app_name,R.string.app_name);
+        drawerToggle.syncState();
+        drawerLayout.addDrawerListener(drawerToggle);
+
+        fragment_months = new Fragment_months();
+        fragment_weeks = new Fragment_weeks();
+        fragment_today = new Fragment_today();
+        fragment_day_add = new Fragment_day_add();
+        getSupportFragmentManager().beginTransaction().replace(R.id.containers,fragment_months).commit();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_bottom);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+           switch(item.getItemId()) {
+               case R.id.cal_months:
+                   getSupportFragmentManager().beginTransaction().replace(R.id.containers,fragment_months).commit();
+                   return true;
+               case R.id.cal_weeks:
+                   getSupportFragmentManager().beginTransaction().replace(R.id.containers,fragment_weeks).commit();
+                   return true;
+               case R.id.cal_today:
+                   getSupportFragmentManager().beginTransaction().replace(R.id.containers,fragment_today).commit();
+                   return true;
+               case R.id.plus:
+                   getSupportFragmentManager().beginTransaction().replace(R.id.containers,fragment_day_add).commit();
+                   return true;
+           }
+            return true;
+        });
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.setting_menu, menu);
+
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.setting_save:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
