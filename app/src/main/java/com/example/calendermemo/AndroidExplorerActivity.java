1053dfb79ed1860635fc2b6433bf7de4c3fe0ba7
DESKTOP-45RVDID\leej <zzckckck3@naver.com>
2022-11-13 17:04:31 +0900

    파일관리자형식 추가

---
+++ app/src/main/java/com/example/calendermemo/AndroidExplorerActivity.java
@@ -0,0 +1,81 @@
+package com.example.calendermemo;
+
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AndroidExplorerActivity extends ListActivity {
+    private List<String> item = null;
+    private List<String> path = null;
+
+    private String root = "/sdcard/";
+    private TextView myPath;
+    @Override
+    public void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.filemain);
+        myPath = (TextView)findViewById(R.id.path);
+        getDir(root);
+    }
+
+    private void getDir(String dirPath){
+        myPath.setText("Location: " + dirPath);
+
+        item = new ArrayList<String>();
+        path = new ArrayList<String>();
+
+        File f = new File(dirPath);
+        File[] files = f.listFiles();
+
+        if(!dirPath.equals(root)){
+            item.add(root);
+            path.add(root);
+            item.add("../");
+            path.add(f.getParent());
+        }
+
+        for(int i = 0; i< files.length;i++){
+            File file = files[i];
+            path.add(file.getPath());
+
+            if(file.isDirectory())
+                item.add(file.getName() + "/");
+            else
+                item.add(file.getName());
+        }
+
+        ArrayAdapter<String> fileList = new ArrayAdapter<String>(this, R.layout.row, item);
+        setListAdapter(fileList);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        File file = new File(path.get(position));
+
+        if(file.isDirectory()){
+            if(file.canRead())
+                getDir(path.get(position));
+            else{
+                new AlertDialog.Builder(this).
+                        setIcon(R.drawable.menu).//버튼 추가(변경)해야함.
+                        setTitle("[" + file.getName() + "]").
+                        setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        }).show();
+            }
+        }
+    }
+
+}
